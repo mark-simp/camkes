723ba38616409dec712ee970f84789f6bea21413
Rod Chapman <rod@proteancode.com>
2022-06-07 15:11:04 +0100

    Update to match order and include comments from the documentation

--- apps/uboot-driver-example/include/plat/odroidc2/platform_devices.h
+++ apps/uboot-driver-example/include/plat/odroidc2/platform_devices.h
@@ -1,17 +1,22 @@
 #pragma once
 
-#define BUS_PATH "/soc/bus@c8100000"
-#define PINCTRL1_PATH "/soc/bus@c8100000/pinctrl@14"
+/* List the set of device tree paths that include the 'reg' entries
+ * for memory regions that will need to be mapped */
+#define BUS_PATH     "/soc/bus@c8100000"
 #define PERIPHS_PATH "/soc/periphs@c8834000"
+#define REG_PATHS { BUS_PATH, PERIPHS_PATH };
+#define REG_PATH_COUNT 2
+
+/* List the set of device tree paths for the devices we wish to access.
+ * Note these need to be the root nodes of each device to be accessed */
+#define PINCTRL1_PATH "/soc/bus@c8100000/pinctrl@14"
 #define PINCTRL2_PATH "/soc/periphs@c8834000/pinctrl@4b0"
-
 #define DEV_PATHS { PINCTRL1_PATH, PINCTRL2_PATH };
-
 #define DEV_PATH_COUNT 2
 
-#define REG_PATHS { BUS_PATH, PERIPHS_PATH };
-
-#define REG_PATH_COUNT 2
+/* Provide the hardware settings for CAmkES. Note that we only need to inform
+ * CAmkES of the devices with memory mapped regions, i.e. the REG_xxx
+ * devices. See https://docs.sel4.systems/projects/camkes for syntax */
 
 #define HARDWARE_INTERFACES                                                   \
     consumes Dummy bus;                                                       \
