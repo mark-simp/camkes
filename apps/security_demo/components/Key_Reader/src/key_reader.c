14adc1824503751a63c778ce34032d9e06ffe9f0
stephen <stephen.gw@gmail.com>
2022-06-22 14:39:09 +0100

    Enable USB keyboard input

--- apps/security_demo/components/Key_Reader/src/key_reader.c
+++ apps/security_demo/components/Key_Reader/src/key_reader.c
@@ -1,21 +1,49 @@
 /*
  * Copyright 2022, Capgemini Engineering
  *
-ß * SPDX-License-Identifier: BSD-2-Clause
+ * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #include <camkes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sel4platsupport/io.h>
+#include <platsupport/delay.h>
 
-int run()
+#include <uboot_drivers.h>
+#include <usb_platform_devices.h>
+
+int run_key_reader(ps_io_ops_t *io_ops)
 {
     printf("Starting Key_Reader\n");
 
-    a_handle_character('a');
-    a_handle_character('b');
-    a_handle_character('c');
+    /* Start the U-Boot driver library */
 
-    printf("Stopping Key_Reader\n");
+    const char *const_reg_paths[] = REG_PATHS;
+    const char *const_dev_paths[] = DEV_PATHS;
+    assert(!initialise_uboot_drivers(
+        /* Provide the platform support IO operations */
+        io_ops,
+        /* List the device tree paths that need to be memory mapped */
+        const_reg_paths, REG_PATH_COUNT,
+        /* List the device tree paths for the devices */
+        const_dev_paths, DEV_PATH_COUNT));
+
+    /* Set USB keyboard as input device */
+    run_uboot_command("setenv stdin usbkbd");
+
+    /* Start the USB subsystem */
+    run_uboot_command("usb start");
+
+    /* Loop forever reading keypresses and passing to the 'handle_character' RPC */
+    while (true)
+    {
+        if (uboot_stdin_tstc() > 0)
+            a_handle_character(uboot_stdin_getc());
+        ps_mdelay(10);
+    }
+
     return 0;
 }
+
+CAMKES_POST_INIT_MODULE_DEFINE(run_key_reader_, run_key_reader);
---
+++ apps/security_demo/include/plat/maaxboard/usb_platform_devices.h
@@ -0,0 +1,70 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+/* List the set of device tree paths that include the 'reg' entries
+ * for memory regions that will need to be mapped */
+
+#define REG_TIMER_PATH      "/soc@0/bus@30400000/timer@306a0000"
+#define REG_CCM_PATH        "/soc@0/bus@30000000/clock-controller@30380000"
+#define REG_SYSCON_PATH     "/soc@0/bus@30000000/syscon@30360000"
+#define REG_USB_2_PATH      "/soc@0/usb@38200000"
+#define REG_USB_PHY_2_PATH  "/soc@0/usb-phy@382f0040"
+
+#define REG_PATH_COUNT 5
+
+#define REG_PATHS {                                                             \
+    REG_USB_2_PATH,                                                             \
+    REG_USB_PHY_2_PATH,                                                         \
+    REG_TIMER_PATH,                                                             \
+    REG_CCM_PATH,                                                               \
+    REG_SYSCON_PATH,                                                            \
+    };
+
+/* List the set of device tree paths for the devices we wish to access.
+ * Note these need ot be the root nodes of each device which the
+ * the library supplies a driver */
+
+#define DEV_TIMER_PATH      REG_TIMER_PATH
+#define DEV_CCM_PATH        REG_CCM_PATH
+#define DEV_SYSCON_PATH     REG_SYSCON_PATH
+#define DEV_USB_2_PATH      REG_USB_2_PATH
+#define DEV_USB_PHY_2_PATH  REG_USB_PHY_2_PATH
+
+#define DEV_PATH_COUNT 5
+
+#define DEV_PATHS {                                                             \
+    DEV_USB_2_PATH,                                                             \
+    DEV_USB_PHY_2_PATH,                                                         \
+    DEV_TIMER_PATH,                                                             \
+    DEV_CCM_PATH,                                                               \
+    DEV_SYSCON_PATH,                                                            \
+    };
+
+/* Provide the hardware settings for CAmkES. Note that we only need to inform
+ * CAmkES with the devices with memory mapped regions, i.e. the REG_xxx
+ * devices. */
+
+#define HARDWARE_INTERFACES                                                     \
+    consumes Dummy usb_2;                                                       \
+    consumes Dummy usb_phy_2;                                                   \
+    consumes Dummy timer;                                                       \
+    consumes Dummy ccm;                                                         \
+    consumes Dummy syscon;                                                      \
+    emits Dummy dummy_source;
+
+#define HARDWARE_COMPOSITION                                                    \
+    connection seL4DTBHardware usb_2_conn(from dummy_source, to usb_2);         \
+    connection seL4DTBHardware usb_phy_2_conn(from dummy_source, to usb_phy_2); \
+    connection seL4DTBHardware timer_conn(from dummy_source, to timer);         \
+    connection seL4DTBHardware ccm_conn(from dummy_source, to ccm);             \
+    connection seL4DTBHardware syscon_conn(from dummy_source, to syscon);
+
+#define HARDWARE_CONFIGURATION                                                  \
+    usb_2.dtb     = dtb({ "path" : REG_USB_2_PATH });                           \
+    usb_phy_2.dtb = dtb({ "path" : REG_USB_PHY_2_PATH });                       \
+    timer.dtb     = dtb({ "path" : REG_TIMER_PATH });                           \
+    ccm.dtb       = dtb({ "path" : REG_CCM_PATH });                             \
+    syscon.dtb    = dtb({ "path" : REG_SYSCON_PATH });
